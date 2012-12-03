ac6272811d493c5438637d2a20e221fcc88ae4db
tanyaobruch1 <another.to@yandex.ru>
2012-12-03 22:41:53 +0200

    Подивіться, будь ласка.
    Коли я обираю "Добавить ссылку на службу" в проекті CalcServiveClient,
    додаю посилання, а проекта не знаходить і виволитьпомилку. Я зробила її
    скріншот.

---
+++ CalculatorService2/CalculatorService2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace CalculatorService2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost sh = new ServiceHost(typeof(CalculatorService));
+            sh.Open();
+            Console.WriteLine("up an running");
+            Console.ReadLine();
+            sh.Close();
+            Console.WriteLine("closed");
+        }
+    }
+}
